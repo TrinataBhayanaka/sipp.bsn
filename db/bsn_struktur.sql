1cb627b53a7a8b03e4b6c7730dd3d2693adb2005
Ovan Pulu <ovan89@gmail.com>
2015-11-15 16:21:27 +0700

    tabel struktur

--- db/bsn_struktur.sql
+++ db/bsn_struktur.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 29, 2015 at 03:54 PM
+-- Generation Time: Nov 15, 2015 at 04:20 PM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.13
+-- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,10 +31,11 @@ CREATE TABLE IF NOT EXISTS `bsn_struktur` (
   `kode` varchar(50) DEFAULT NULL,
   `nama_satker` varchar(300) DEFAULT NULL,
   `singkatan` varchar(100) DEFAULT NULL,
+  `type` int(11) NOT NULL COMMENT '1=lembaga,2=eselon1,3=eselon2',
   `create_date` datetime DEFAULT NULL,
   `n_status` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
