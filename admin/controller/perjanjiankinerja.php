f1605fad9b63a787015a73050c8a13a91bce9af7
andreassbayu <andreass.bayu@gmail.com>
2015-11-30 19:04:22 +0000

    pk all

--- admin/controller/perjanjiankinerja.php
+++ admin/controller/perjanjiankinerja.php
@@ -90,16 +90,29 @@ class perjanjiankinerja extends Controller {
 
 	public function eselon1()
 	{
-		$struktur = $this->model->getStruktur(2);
+		$es = $_GET['tp'];
+		$struktur = $this->model->getStruktur($es);
+		if($es == '2')$labelEs="I";else $labelEs="II";
+		$this->view->assign('labelEs',$labelEs);
+		$this->view->assign('es',$es);
 
 		if(!$_POST) {
-			$idpk = $struktur[0]['kode']; 
+			$idpk = $struktur[0]['kode'];
+			$parent = $struktur[0]['id'];
+			$this->view->assign('label',$struktur[0]['nama_satker']);
+			$this->view->assign('id',$struktur[0]['id']);
+			$this->view->assign('idpk',$idpk);
 		} else {
 			$exp = explode("_", $_POST['struktur']);
 			$idpk = $exp[2];
+			$parent = $exp[0];
+			$this->view->assign('label',$exp[1]);
+			$this->view->assign('id',$exp[0]);
+			$this->view->assign('idpk',$idpk);
 		}
 
-		$data = $this->model->getpk($idpk,1);
+		$data = $this->model->getpk($idpk,$parent);
+
 		
 		$this->view->assign('data',$data);
 		$this->view->assign('struktur',$struktur);
@@ -107,6 +120,77 @@ class perjanjiankinerja extends Controller {
 		return $this->loadView('pk/eselon1');
 	}
 
+	public function add_eselon()
+	{
+		$ss = $this->getSS($_GET['id']);
+
+		$this->view->assign('ss',$ss);
+		$this->view->assign('idpk',$_GET['kd']);
+
+		return $this->loadView('pk/add_eselon');		
+	}
+
+	public function ins_pk_eselon()
+	{
+		global $basedomain;
+		
+		$this->model->insert_pk($_POST);
+
+		echo "<script>alert('Data berhasil masuk');window.location.href='".$basedomain."perjanjiankinerja/eselon1'</script>";
+		exit;
+	}
+
+	public function edit_eselon()
+	{
+
+		$data = $this->model->getpk($_GET['kd'],$_GET['pr'],$_GET['id']);
+		$ss = $this->getSS($_GET['pr']);
+
+		$this->view->assign('ss',$ss);
+		$this->view->assign('data',$data[0]);
+		$this->view->assign('kode',$_GET['kd']);
+
+		return $this->loadView('pk/edit_eselon');
+	}
+
+	public function edt_pk_eselon()
+	{
+		global $basedomain;
+
+		$this->model->edit_pk($_POST);
+
+		echo "<script>alert('Data berhasil dirubah');window.location.href='".$basedomain."perjanjiankinerja/eselon1'</script>";
+		exit;
+	}
+
+	public function eselon2()
+	{
+		$struktur = $this->model->getStruktur(3);
+		$this->view->assign('eselon','II');
+
+		if(!$_POST) {
+			$idpk = $struktur[0]['kode'];
+			$parent = $struktur[0]['id'];
+			$this->view->assign('label',$struktur[0]['nama_satker']);
+			$this->view->assign('id',$struktur[0]['id']);
+			$this->view->assign('idpk',$idpk);
+		} else {
+			$exp = explode("_", $_POST['struktur']);
+			$idpk = $exp[2];
+			$parent = $exp[0];
+			$this->view->assign('label',$exp[1]);
+			$this->view->assign('id',$exp[0]);
+			$this->view->assign('idpk',$idpk);
+		}
+
+		$data = $this->model->getpk($idpk,$parent);
+
+		
+		$this->view->assign('data',$data);
+		$this->view->assign('struktur',$struktur);
+
+		return $this->loadView('pk/eselon2');
+	}
 	
 }
 
